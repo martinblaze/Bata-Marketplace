c3f50869e616c49a56abd8a46666111bf499a7f6
Martin <martinchidozie27@gmail.com>
2026-02-26 19:25:10 +0100

    fix: disable static bailout for CSR pages

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     domains: ['localhost', 'res.cloudinary.com', 'lh3.googleusercontent.com'],
   },
 
+  // Ignore all build-stopping errors
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -14,6 +15,9 @@ const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
+
+  // Never try to statically generate pages at build time
+  staticPageGenerationTimeout: 0,
 };
 
 module.exports = nextConfig;
