0f1149def3f8c073f352260fec006145eec254db
Martin <martinchidozie27@gmail.com>
2026-03-16 10:47:29 +0100

    new modification

--- app/page.tsx
+++ app/page.tsx
@@ -708,7 +708,7 @@ export default function HomePage() {
               <div className="col-span-2 md:col-span-1">
                 <div className="flex items-center mb-4">
                   <Image
-                    src="/BATAMARTMART - logo.png"
+                    src="/BATAMART - logo.png"
                     alt="BATAMART Logo"
                     width={400}
                     height={400}
